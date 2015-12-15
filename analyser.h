c8d52d4f088f80cf775c1ef0b3b8e8b047181357
xinarivar <ryzhov.denis@gmail.com>
2015-12-16 00:42:52 +0300

    Add some comments.

--- analyser.h
+++ analyser.h
@@ -9,16 +9,17 @@ namespace fs = boost::filesystem;
 
 class Analyser {
 public:
+	// Recursively browse directory and build inlcuded files tree for all '.cpp' files.
 	static void Analyze(FileForest& forest, fs::path directory) {
 		fs::directory_iterator it(directory), end;
 
 		for (; it != end; ++it) {
 			if (is_regular_file(it->path())) {
 				if (it->path().has_extension() && it->path().extension() == ".cpp") {
-					std::cout << it->path().string() << std::endl;
 					forest.BuildTree(it->path());
 				}
 			} else if (is_directory(it->path())) {
+				// Browse subdirectory
 				Analyze(forest, it->path());
 			}
 		}
--- forest.cpp
+++ forest.cpp
@@ -1,6 +1,7 @@
 
 #include "forest.h"
 
+// Add path for included files search
 bool FileForest::AddIncludePath (const fs::path directory) {
 	if (exists(directory) && is_directory(directory)) {
 		pathInclude.push_back(directory);
@@ -11,22 +12,27 @@ bool FileForest::AddIncludePath (const fs::path directory) {
 	return false;
 }
 
+// Build files tree in 'listFiled' map variable.
 void FileForest::BuildTree (const fs::path file) {
 	if (exists(file) && is_regular_file(file)) {
+		// Do not inspect file twice
 		map< fs::path, vector<fs::path> >::iterator found = listFiles.find(file);
 		if (found != listFiles.end()) return;
 
+		// Parse file for all '#include' directives
 		vector<string> filesInclude = IncludeParser::Parse(file.string());
 
 		fs::path tmp;
 		vector<fs::path> listTmp;
 
 		for (vector<string>::iterator it(filesInclude.begin()); it != filesInclude.end(); ++it) {
+			// find file in current dir
 			if (it->at(0) == '\"') {
 				tmp = file.parent_path() / it->substr(1, it->length() - 2);
 				if (exists(tmp) && is_regular_file(tmp)) {
 					listTmp.push_back(tmp);
-				}
+				} else listTmp.push_back(tmp);
+			// find file in included dirs
 			} else if (it->at(0) == '<') {
 				bool deleted = true;
 				
@@ -39,6 +45,7 @@ void FileForest::BuildTree (const fs::path file) {
 					}
 				}	// for jt
 
+				// if file not exist? add path in tree anyway
 				if (deleted == true) {
 					tmp = it->substr(1, it->length() - 2);
 					listTmp.push_back(tmp);
@@ -46,25 +53,31 @@ void FileForest::BuildTree (const fs::path file) {
 			}
 		}	// for it
 
+		// add current and included files in tree
 		listFiles.insert( pair< fs::path, vector<fs::path> >(file, listTmp) );
-		
+
+		// construct other part of tree		
 		for (vector<fs::path>::iterator it(listTmp.begin()); it != listTmp.end(); ++it) {
 			BuildTree(*it);
 		}
 	}
 }
 
+// Couning of included files frequency
 void FileForest::AddFrequency (const fs::path file) {
 	int init = 1;
 	map<fs::path, unsigned int>::iterator freq =  listFreq.find(file);
 
 	if (freq == listFreq.end()) {
+		// root file, not included
 		if (file.extension() == ".cpp") init = 0;
 		listFreq.insert(pair<fs::path, unsigned int>(file, init));
 	} else freq->second++;
 }
 
+// Recursive print tree function
 void FileForest::PrintTree (vector<fs::path>& parent) {
+	// Depth include counting
 	string dots;
 	for (int i = 0; i < parent.size() - 1; ++i) {
 		dots += "..";
@@ -77,6 +90,7 @@ void FileForest::PrintTree (vector<fs::path>& parent) {
 
 		vector<fs::path>::const_iterator it(found->second.begin());
 		for (;it != found->second.end(); ++it) {
+			// if file is in vector 'parent', then it cycled
 			vector<fs::path>::iterator cycle = find(parent.begin(), parent.end(), *it);
 			if (cycle == parent.end()) {
 				parent.push_back(*it);
@@ -87,14 +101,17 @@ void FileForest::PrintTree (vector<fs::path>& parent) {
 			}
 		}
 	} else {
+		// deletd files
 		AddFrequency(parent.back());
 		std::cout << dots << parent.back().filename().string() << " (!)" << std::endl;
 	}
 }
 
+// Common print forest of 'cpp' files
 void FileForest::Print () {
 	listFreq.clear();
 
+	// Print forest
 	vector<fs::path> tree;
 	map< fs::path, vector<fs::path> >::const_iterator it(listFiles.begin());
 	for (; it != listFiles.end(); ++it) {
@@ -105,6 +122,7 @@ void FileForest::Print () {
 		}
 	}
 
+	// Print included frequency
 	std::cout << std::endl;
 	map<fs::path, unsigned int>::const_iterator jt(listFreq.begin());
 	for (; jt != listFreq.end(); ++jt) {
